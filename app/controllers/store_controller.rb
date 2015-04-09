6e710fd98e5e5a81beaa81b064eadee12b270e3a
scottnice <scottnice@live.com>
2015-04-09 17:09:42 -0400

    Added a cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,5 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    puts @products.inspect
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_reference_by_any_line_item
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -7,5 +13,15 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
-  validates :title, length: {minimum: 10, too_short: "must have at least 10 letters",}
+  validates :title, length: {minimum: 10, too_short: "must have at least 10 letters"}
+
+  private
+  def ensure_not_reference_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
